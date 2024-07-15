4943bcb433b2b648b954594a7895939f21a2b7b4
Marcos Paulo <marcos.poo@gmail.com>
2024-07-15 17:00:44 -0300

    Aula 1 - O pontapé de nossa aplicação Web

--- ByteBank.Portal/Program.cs
+++ ByteBank.Portal/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ByteBank.Portal.Infraestrutura;
 
 namespace ByteBank.Portal
 {
@@ -10,6 +6,10 @@ namespace ByteBank.Portal
     {
         static void Main(string[] args)
         {
+            var prefixos = new string[] {"http://localhost:5341/"};
+            var webApplication = new WebApplication(prefixos);
+            webApplication.Iniciar();
+
         }
     }
 }
